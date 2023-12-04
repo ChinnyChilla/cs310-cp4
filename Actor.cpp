077ad0db3317874d742ea3e626d1fe5d3c32fb05
wreilly1 <113715123+wreilly1@users.noreply.github.com>
2023-12-04 14:58:12 -0500

    Update Actor.cpp

--- Actor.cpp
+++ Actor.cpp
@@ -12,24 +12,11 @@ Actor::Actor(unsigned int act_id, string lname, string fname) {
 	actor_id = act_id;
 }
 
-bool Actor::addMovie(const unsigned int movieid) {
-	for (unsigned int i = 0; i < movies.size(); i++) {
-		if (movies[i] == movieid)
-			return false;
-	}
-	movies.push_back(movieid);
-	return true;
-}
+bool Actor::operator==(const Actor& other) {
+        return actor_id == other.actor_id; 
+    }
 
-/*
-void Actor::showCareer(const MovieDB &moviedb) const {
-	string movietitle;
-	cout << first << " " << last << " has acted in:" << endl;
-	for (unsigned int i = 0; i < movies.size(); i++) {
-		if (moviedb.find(movies[i])) {
-			movietitle = moviedb.getTitle(movies[i].getID());
-			cout << "- " << movietitle << endl;
-		}
-	}
-}
-*/
+bool Actor::operator!=(const Actor& other){
+        return !(*this == other);
+    }
+
