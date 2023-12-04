d36f739fde8b49021d33d6668ae43ead75b81022
wreilly1 <113715123+wreilly1@users.noreply.github.com>
2023-12-04 15:00:22 -0500

    Update Actor.h

--- Actor.h
+++ Actor.h
@@ -2,27 +2,30 @@
 #define __ACTOR_H_
 
 #include <iostream>
+#include "Array.h"
 using namespace std;
-#include <vector>
 
-//#include "MovieDB.h"
+
+
 
 class Actor {
 	private:
 		unsigned int actor_id;
 		string last;
 		string first;
-		vector<unsigned int> movies;		
 	public:
 		Actor();
 		Actor(unsigned int act_id, string last, string first); 
-		bool addMovie(const unsigned int movie_id);
-		//void showCareer(const MovieDB &moviedb) const;
-
+     bool operator==(const Actor& other);
+     bool operator!=(const Actor& other);
 		string getName() const { 
 			return first + " " + last;
 		}
 		unsigned int getID() const { return actor_id; }
+	      string getLastName() const{
+	                 return last;
+                }
+
 };
 
 #endif
